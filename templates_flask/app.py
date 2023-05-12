49710364bfccab426dc9be2aef2996677c94816d
claudiaastefanelli <stefanelli.claudia@iisgalvanimi.edu.it>
2023-05-12 09:46:37 +0000

    faccia da cazo

---
+++ templates_flask/app.py
@@ -0,0 +1,12 @@
+from flask import render_template
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/units')
+def unitList():
+    mycursor.execute("SELECT * FROM StudentsPerformance")
+    myresult = mycursor.fetchall()
+    return render_template('hello.html', units=myresult)
